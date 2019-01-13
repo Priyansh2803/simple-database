18874208c34e88b13f1a0c495e73990a4555f03e
Priyansh Nema <43186210+Priyansh2803@users.noreply.github.com>
2019-01-14 00:49:50 +0530

    Add files via upload

---
+++ comma.py
@@ -0,0 +1,6 @@
+First_Name, Last_Name, Phone_no, Email_id, = input("Enter Your Data Saperated by comma ").split(",")
+
+Full_Name = (First_Name + Last_Name)
+print(Full_Name)
+print(Phone_no)
+print(Email_id)
